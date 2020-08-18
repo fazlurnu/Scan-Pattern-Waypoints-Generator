c551902382f6d07e6fa8b114106ddfc711b099e3
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-08-18 23:16:46 +0800

    add outmost points

--- waypoint_generator.py
+++ waypoint_generator.py
@@ -37,29 +37,59 @@ def getOrientation(ROI):
 def getGradient(orientation):
     return tan(orientation)
     
-def getFurthestX(ROI):
-    furthestX = 0
+def getMaxX(ROI):
+    maxX = 0
     for point in ROI:
-        if(point[0] > furthestX):
-            furthestX = point[0]
+        if(point[0] > maxX):
+            maxX = point[0]
             
-    return furthestX
+    return maxX
 
-def drawScanLines(ROI, gradient):
-    for i in range(5):    
+def getMinX(ROI):
+    minX = 1000
+    for point in ROI:
+        if(point[0] < minX):
+            minX = point[0]
+            
+    return minX
+
+def getMaxY(ROI):
+    maxY = 0
+    for point in ROI:
+        if(point[1] > maxY):
+            maxY = point[1]
+            
+    return maxY
+
+def getMinY(ROI):
+    minY = 1000
+    for point in ROI:
+        if(point[1] < minY):
+            minY = point[1]
+            
+    return minY
+
+def getScanLines(ROI, gradient):
+    scanLines = []
+    
+    for i in range(6):    
         x1 = ROI[0][0]
         y1 = ROI[0][0]+clearance*(i+1)
         point1 = (x1, y1)
-        x2 = getFurthestX(ROI)
+        x2 = getMaxX(ROI)
         point2 = (x2, gradient*x2 + clearance*(i+1))
         
         x = [point1[0], point2[0]]
         y = [point1[1], point2[1]]
         
+        scanLines.append((point1, point2))
+        
         plt.plot(x, y, "-b.", linewidth = 2, markersize = 10)
         plt.title("Grid Scan Waypoint Generator")
         plt.xlabel("x-axis")
         plt.ylabel("y-axis")
+    
+    return scanLines
         
 def main(ROI, orientation = None):
     
@@ -75,8 +105,9 @@ def main(ROI, orientation = None):
     gradient = getGradient(orientation)
     
     drawROI(ROI)
-    drawScanLines(ROI, gradient)    
+    scanLines = getScanLines(ROI, gradient)    
+    print(scanLines)
     
 if __name__ == "__main__":
-    ROI = [(0,0), (1,1.2), (2.1,3), (0.75,3), (0,2)]
-    main(ROI)
+    ROI = [(0,0), (1,1.2), (2.1,3), (0.75,4), (0,2)]
+    main(ROI, orientation=deg2rad(20))
