f8478bace4beaf41e3d080f642d69066bd8c0b70
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-08-18 22:36:15 +0800

    add orientation

--- waypoint_generator.py
+++ waypoint_generator.py
@@ -6,6 +6,8 @@ Created on Tue Aug 18 22:11:01 2020
 """
 
 import matplotlib.pyplot as plt
+from math import atan2
+from numpy import rad2deg, deg2rad
 
 def drawROI(ROI):
     x = []
@@ -20,13 +22,29 @@ def drawROI(ROI):
     plt.xlabel("x-axis")
     plt.ylabel("y-axis")
     
-def main(ROI):
+def getOrientation(ROI):
+    point1 = ROI[0]
+    point2 = ROI[1]
+    diffX = point2[0] - point1[0]
+    diffY = point2[1] - point1[1]
+    
+    orientation = atan2(diffY, diffX)
+    
+    return orientation
+    
+def main(ROI, orientation=None):
     
     if (ROI[-1] != ROI[0]):
         ROI.insert(len(ROI), ROI[0])
         
+    if(orientation==None):
+        orientation = getOrientation(ROI)
+        print(rad2deg(orientation))
+    else:
+        print(orientation)
+        
     drawROI(ROI)
     
 if __name__ == "__main__":
-    ROI = [(0,0), (0,1), (1,2), (2,3)]
+    ROI = [(0,0), (1,1), (2,3), (1,3), (0,2)]
     main(ROI)
